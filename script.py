c6a61891fd4a584cd842f6925cd84faf25b81267
Apple Macbook Air M1 <applemacbookairm1@Apples-MacBook-Air.local>
2022-09-07 21:02:34 +0400

    test2

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 from twilio.twiml.voice_response import VoiceResponse
 
-account_sid = 'ACab1199d1213963e4669ea072dcb7e8a5'
-auth_token = 'cc1e5ca6a303a17dd3b021f49b56e5e2'
+account_sid = 'ACab1199d1213963e4669ea072dcb7e8a5' 
+auth_token = '316ff6c6207783e2728a5ef3960d1913'
 
 client = Client(account_sid, auth_token)
 
