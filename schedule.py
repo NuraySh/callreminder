e84f6376c0203d0ad0fda0856c50de3ade6f4942
Apple Macbook Air M1 <applemacbookairm1@Apples-MacBook-Air.local>
2022-09-07 20:46:14 +0400

    test

--- schedule.py
+++ schedule.py
@@ -9,7 +9,7 @@ aze = pytz.timezone('Asia/Baku')
 
 schedule = BlockingScheduler()
 
-trigger = OrTrigger([CronTrigger(day_of_week='mon-sun', hour='11', minute='05'),CronTrigger(day_of_week='mon-sun', hour='20', minute='23')])
+trigger = OrTrigger([CronTrigger(day_of_week='mon-sun', hour='11', minute='05'),CronTrigger(day_of_week='mon-sun', hour='20', minute='46')])
 
 
 schedule.add_job(callmsg, trigger, timezone=aze)
